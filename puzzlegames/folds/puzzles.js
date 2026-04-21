52dddb4c050640840f2c574d265d6fd5acffd2a4
jbatterson <jason.batterson@gmail.com>
2026-04-20 23:19:21 -0700

    new hard folds and scurry

--- puzzlegames/folds/puzzles.js
+++ puzzlegames/folds/puzzles.js
@@ -2052,9 +2052,11 @@ export default {
       folds: 3,
     },
 
-
-
-
+    {
+      start: { '2,5': 'amber', '2,6': 'amber', '3,6': 'dkblue', '5,3': 'amber' },
+      target: { '0,5': 'amber', '0,6': 'amber', '1,1': 'amber', '1,5': 'amber', '1,6': 'dkblue', '2,1': 'amber', '2,2': 'dkblue', '2,3': 'amber', '2,5': 'amber', '2,6': 'amber', '2,7': 'amber', '3,3': 'amber', '3,6': 'dkblue', '3,7': 'dkblue', '3,8': 'amber', '3,9': 'amber', '4,3': 'amber', '4,6': 'dkblue', '4,7': 'dkblue', '4,8': 'amber', '4,9': 'amber', '5,1': 'amber', '5,2': 'dkblue', '5,3': 'amber', '5,5': 'amber', '5,6': 'amber', '5,7': 'amber', '6,1': 'amber', '6,5': 'amber', '6,6': 'dkblue', '7,5': 'amber', '7,6': 'amber' },
+      folds: 3,
+    },
 
 
     {
@@ -2076,6 +2078,18 @@ export default {
     },
 
     {
+      start: { '2,2': 'blue', '3,2': 'blue', '4,2': 'blue', '4,3': 'red' },
+      target: { '1,1': 'blue', '1,2': 'blue', '1,3': 'red', '2,1': 'red', '2,2': 'blue', '2,3': 'blue', '2,4': 'blue', '3,2': 'blue', '3,3': 'red', '3,5': 'red', '3,6': 'blue', '4,2': 'blue', '4,3': 'red', '4,5': 'red', '4,6': 'blue', '5,1': 'red', '5,2': 'blue', '5,3': 'blue', '5,4': 'blue', '6,1': 'blue', '6,2': 'blue', '6,3': 'red' },
+      folds: 3,
+    },
+
+    {
+      start: { '1,1': 'amber', '2,1': 'amber', '2,7': 'dkblue', '2,8': 'amber' },
+      target: { '0,6': 'amber', '1,1': 'amber', '1,4': 'amber', '1,5': 'dkblue', '1,6': 'dkblue', '1,7': 'dkblue', '1,8': 'amber', '1,9': 'amber', '2,1': 'amber', '2,7': 'dkblue', '2,8': 'amber', '3,1': 'amber', '3,7': 'dkblue', '3,8': 'amber', '4,1': 'amber', '4,4': 'amber', '4,5': 'dkblue', '4,6': 'dkblue', '4,7': 'dkblue', '4,8': 'amber', '4,9': 'amber', '5,6': 'amber' },
+      folds: 3,
+    },
+
+    {
       start: { '1,8': 'amber', '3,8': 'dkblue', '4,1': 'amber', '4,2': 'amber' },
       target: { '0,4': 'amber', '0,5': 'amber', '0,6': 'amber', '1,3': 'dkblue', '1,8': 'amber', '1,9': 'amber', '2,4': 'amber', '2,5': 'amber', '2,6': 'amber', '2,9': 'amber', '3,1': 'amber', '3,2': 'amber', '3,3': 'dkblue', '3,4': 'dkblue', '3,8': 'dkblue', '4,1': 'amber', '4,2': 'amber', '4,3': 'dkblue', '4,4': 'dkblue', '4,8': 'dkblue', '5,4': 'amber', '5,5': 'amber', '5,6': 'amber', '5,9': 'amber', '6,3': 'dkblue', '6,8': 'amber', '6,9': 'amber', '7,4': 'amber', '7,5': 'amber', '7,6': 'amber' },
       folds: 3,
@@ -2088,8 +2102,32 @@ export default {
     },
 
     {
+      start: { '2,3': 'red', '4,4': 'red', '5,3': 'red', '5,4': 'amber', '6,3': 'red' },
+      target: { '0,1': 'red', '0,2': 'red', '1,2': 'amber', '1,3': 'red', '1,5': 'red', '2,1': 'red', '2,2': 'red', '2,3': 'red', '2,7': 'red', '3,0': 'red', '3,2': 'amber', '3,3': 'red', '3,7': 'red', '3,8': 'red', '3,9': 'amber', '3,10': 'red', '3,11': 'red', '4,2': 'amber', '4,3': 'red', '4,4': 'red', '4,5': 'red', '4,6': 'amber', '4,7': 'red', '4,8': 'red', '5,1': 'red', '5,2': 'red', '5,3': 'red', '5,4': 'amber', '5,7': 'red', '6,0': 'red', '6,1': 'amber', '6,2': 'amber', '6,3': 'red', '7,0': 'red', '7,1': 'red', '7,2': 'red' },
+      folds: 3,
+    },
+
+    {
       start: { '2,1': 'green', '2,2': 'amber', '5,0': 'green', '5,1': 'green' },
       target: { '1,3': 'green', '1,4': 'green', '2,0': 'green', '2,1': 'green', '2,2': 'amber', '2,3': 'amber', '2,6': 'green', '2,7': 'green', '2,8': 'green', '3,6': 'amber', '3,7': 'amber', '3,8': 'green', '3,11': 'green', '4,0': 'green', '4,1': 'green', '4,2': 'amber', '4,10': 'amber', '4,11': 'green', '5,0': 'green', '5,1': 'green', '5,2': 'amber', '7,0': 'green', '7,1': 'green', '7,2': 'amber' },
+      folds: 3,
+    },
+
+    {
+      start: { '4,1': 'blue', '4,2': 'blue', '7,0': 'blue', '7,1': 'blue', '7,2': 'amber' },
+      target: { '1,3': 'blue', '1,4': 'blue', '1,5': 'amber', '2,0': 'blue', '2,1': 'blue', '2,6': 'blue', '3,0': 'amber', '3,3': 'blue', '3,4': 'blue', '3,5': 'blue', '3,6': 'blue', '3,10': 'amber', '3,11': 'blue', '4,1': 'blue', '4,2': 'blue', '4,3': 'blue', '4,4': 'blue', '4,5': 'blue', '4,6': 'blue', '4,8': 'blue', '4,10': 'amber', '4,11': 'blue', '5,4': 'blue', '5,5': 'blue', '5,6': 'blue', '5,7': 'amber', '5,8': 'blue', '6,0': 'amber', '6,3': 'blue', '6,4': 'blue', '6,5': 'amber', '7,0': 'blue', '7,1': 'blue', '7,2': 'amber' },
+      folds: 3,
+    },
+
+    {
+      start: { '2,0': 'red', '3,-1': 'red', '3,0': 'red', '5,1': 'red', '5,2': 'red' },
+      target: { '2,0': 'red', '2,1': 'red', '2,2': 'red', '2,3': 'red', '3,-1': 'red', '3,0': 'red', '3,3': 'red', '3,4': 'red', '3,5': 'red', '4,0': 'red', '4,1': 'red', '4,2': 'red', '4,3': 'red', '4,4': 'red', '4,5': 'red', '5,0': 'red', '5,1': 'red', '5,2': 'red', '5,3': 'red', '5,4': 'red', '5,5': 'red', '5,7': 'red', '5,8': 'red', '6,3': 'red', '6,4': 'red', '6,5': 'red', '6,7': 'red', '7,1': 'red', '7,2': 'red', '7,3': 'red' },
+      folds: 3,
+    },
+
+    {
+      start: { '5,9': 'red', '7,6': 'blue', '7,7': 'blue', '7,8': 'blue' },
+      target: { '0,4': 'red', '0,6': 'blue', '0,7': 'blue', '0,8': 'blue', '1,8': 'blue', '2,0': 'blue', '2,1': 'blue', '2,4': 'red', '2,7': 'blue', '2,8': 'blue', '2,9': 'red', '3,0': 'blue', '3,2': 'red', '3,8': 'blue', '4,0': 'blue', '4,2': 'red', '4,8': 'blue', '5,0': 'blue', '5,1': 'blue', '5,4': 'red', '5,7': 'blue', '5,8': 'blue', '5,9': 'red', '6,8': 'blue', '7,4': 'red', '7,6': 'blue', '7,7': 'blue', '7,8': 'blue' },
       folds: 3,
     },
 
@@ -2112,6 +2150,24 @@ export default {
     },
 
     {
+      start: { '3,4': 'dkblue', '3,5': 'dkblue', '4,2': 'dkblue', '4,3': 'dkblue', '4,4': 'dkblue' },
+      target: { '2,0': 'dkblue', '2,2': 'dkblue', '2,3': 'dkblue', '3,0': 'dkblue', '3,2': 'dkblue', '3,4': 'dkblue', '3,5': 'dkblue', '3,7': 'dkblue', '4,0': 'dkblue', '4,2': 'dkblue', '4,3': 'dkblue', '4,4': 'dkblue', '4,6': 'dkblue', '4,7': 'dkblue', '4,8': 'dkblue', '5,0': 'dkblue', '5,2': 'dkblue', '5,3': 'dkblue', '5,4': 'dkblue', '5,6': 'dkblue', '5,7': 'dkblue', '5,8': 'dkblue', '6,0': 'dkblue', '6,2': 'dkblue', '6,4': 'dkblue', '6,5': 'dkblue', '6,7': 'dkblue', '7,0': 'dkblue', '7,2': 'dkblue', '7,3': 'dkblue' },
+      folds: 3,
+    },
+
+    {
+      start: { '2,4': 'blue', '6,6': 'blue', '7,5': 'blue', '7,6': 'amber', '7,7': 'blue' },
+      target: { '0,4': 'blue', '0,5': 'blue', '0,6': 'amber', '0,7': 'blue', '1,1': 'blue', '1,2': 'blue', '1,6': 'blue', '2,0': 'blue', '2,1': 'amber', '2,2': 'blue', '2,4': 'blue', '2,5': 'blue', '2,6': 'amber', '2,7': 'blue', '2,8': 'blue', '3,6': 'blue', '3,7': 'blue', '3,8': 'amber', '3,9': 'blue', '4,6': 'blue', '4,7': 'blue', '4,8': 'amber', '4,9': 'blue', '5,0': 'blue', '5,1': 'amber', '5,2': 'blue', '5,4': 'blue', '5,5': 'blue', '5,6': 'amber', '5,7': 'blue', '5,8': 'blue', '6,1': 'blue', '6,2': 'blue', '6,6': 'blue', '7,4': 'blue', '7,5': 'blue', '7,6': 'amber', '7,7': 'blue' },
+      folds: 3,
+    },
+
+    {
+      start: { '3,1': 'red', '4,1': 'red', '5,6': 'red', '5,7': 'amber', '6,7': 'red' },
+      target: { '0,2': 'amber', '0,3': 'red', '1,2': 'red', '1,5': 'amber', '1,6': 'red', '1,7': 'red', '2,4': 'red', '2,5': 'red', '2,6': 'red', '2,7': 'amber', '2,10': 'red', '3,1': 'red', '3,2': 'red', '3,3': 'red', '3,4': 'red', '3,9': 'red', '3,10': 'amber', '4,1': 'red', '4,2': 'red', '4,3': 'red', '4,4': 'red', '4,9': 'red', '4,10': 'amber', '5,4': 'red', '5,5': 'red', '5,6': 'red', '5,7': 'amber', '5,10': 'red', '6,2': 'red', '6,5': 'amber', '6,6': 'red', '6,7': 'red', '7,2': 'amber', '7,3': 'red' },
+      folds: 3,
+    },
+
+    {
       start: { '2,1': 'blue', '2,2': 'dkblue', '3,2': 'dkblue', '5,2': 'dkblue' },
       target: { '0,3': 'dkblue', '0,6': 'blue', '1,3': 'blue', '1,4': 'blue', '1,5': 'dkblue', '1,6': 'dkblue', '1,7': 'dkblue', '1,8': 'blue', '2,1': 'blue', '2,2': 'dkblue', '2,3': 'dkblue', '2,4': 'dkblue', '2,5': 'dkblue', '2,6': 'dkblue', '2,7': 'dkblue', '2,8': 'blue', '2,10': 'dkblue', '3,2': 'dkblue', '3,3': 'dkblue', '3,6': 'dkblue', '3,7': 'dkblue', '3,8': 'blue', '4,5': 'dkblue', '4,6': 'dkblue', '4,7': 'dkblue', '4,8': 'blue', '5,2': 'dkblue', '5,3': 'dkblue' },
       folds: 3,
@@ -2126,6 +2182,12 @@ export default {
     {
       start: { '2,8': 'red', '2,9': 'red', '4,5': 'dkblue', '4,6': 'dkblue' },
       target: { '1,5': 'dkblue', '1,6': 'dkblue', '2,2': 'dkblue', '2,3': 'dkblue', '2,8': 'red', '2,9': 'red', '3,5': 'dkblue', '3,6': 'dkblue', '3,7': 'dkblue', '3,8': 'red', '3,9': 'red', '4,2': 'red', '4,3': 'red', '4,4': 'red', '4,5': 'dkblue', '4,6': 'dkblue', '4,7': 'dkblue', '4,8': 'red', '4,9': 'red', '4,10': 'dkblue', '5,4': 'red', '5,5': 'red', '5,6': 'red', '5,7': 'red', '5,8': 'red', '5,9': 'red', '5,10': 'dkblue', '6,5': 'dkblue', '6,6': 'dkblue', '6,7': 'red' },
+      folds: 3,
+    },
+
+    {
+      start: { '3,2': 'blue', '3,3': 'blue', '4,3': 'blue', '6,7': 'red' },
+      target: { '0,3': 'red', '1,6': 'red', '1,7': 'red', '2,4': 'blue', '2,5': 'blue', '2,6': 'blue', '2,10': 'red', '3,1': 'blue', '3,2': 'blue', '3,3': 'blue', '3,4': 'blue', '3,5': 'blue', '3,6': 'blue', '4,1': 'blue', '4,2': 'blue', '4,3': 'blue', '4,4': 'blue', '4,5': 'blue', '4,6': 'blue', '5,4': 'blue', '5,5': 'blue', '5,6': 'blue', '5,10': 'red', '6,6': 'red', '6,7': 'red', '7,3': 'red' },
       folds: 3,
     },
 
@@ -2168,6 +2230,12 @@ export default {
     {
       start: { '0,8': 'dkblue', '1,1': 'dkblue', '2,1': 'dkblue', '4,5': 'dkblue', '4,6': 'amber' },
       target: { '0,7': 'dkblue', '0,8': 'dkblue', '1,1': 'dkblue', '1,5': 'dkblue', '1,6': 'amber', '1,8': 'dkblue', '1,9': 'dkblue', '2,1': 'dkblue', '2,2': 'amber', '2,3': 'dkblue', '2,7': 'dkblue', '2,8': 'dkblue', '3,1': 'dkblue', '3,2': 'amber', '3,3': 'dkblue', '3,5': 'dkblue', '3,6': 'amber', '3,8': 'dkblue', '3,9': 'dkblue', '4,1': 'dkblue', '4,2': 'amber', '4,3': 'dkblue', '4,5': 'dkblue', '4,6': 'amber', '4,8': 'dkblue', '4,9': 'dkblue', '5,1': 'dkblue', '5,2': 'amber', '5,3': 'dkblue', '5,7': 'dkblue', '5,8': 'dkblue', '6,1': 'dkblue', '6,5': 'dkblue', '6,6': 'amber', '6,8': 'dkblue', '6,9': 'dkblue', '7,7': 'dkblue', '7,8': 'dkblue' },
+      folds: 3,
+    },
+
+    {
+      start: { '0,5': 'dkblue', '0,6': 'dkblue', '1,2': 'dkblue', '1,3': 'dkblue', '2,3': 'dkblue' },
+      target: { '0,5': 'dkblue', '0,6': 'dkblue', '1,2': 'dkblue', '1,3': 'dkblue', '1,8': 'dkblue', '1,9': 'dkblue', '2,3': 'dkblue', '2,4': 'dkblue', '2,5': 'dkblue', '2,6': 'dkblue', '3,1': 'dkblue', '3,3': 'dkblue', '3,4': 'dkblue', '3,5': 'dkblue', '3,6': 'dkblue', '3,7': 'dkblue', '3,8': 'dkblue', '3,9': 'dkblue', '4,1': 'dkblue', '4,2': 'dkblue', '4,3': 'dkblue', '4,4': 'dkblue', '4,5': 'dkblue', '5,4': 'dkblue', '5,5': 'dkblue', '5,6': 'dkblue' },
       folds: 3,
     },
   ],
